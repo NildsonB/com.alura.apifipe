9e412a8ac4a8fcee56d4607a43ecaec503bfa1de
Nildson <nildsonb15@gmail.com>
2026-06-11 00:25:13 -0300

    first commit

---
+++ src/main/java/com/alura/apifipe/ApifipeApplication.java
@@ -0,0 +1,13 @@
+package com.alura.apifipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/apifipe/ApifipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.apifipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
