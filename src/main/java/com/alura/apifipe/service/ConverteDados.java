21448b0172ea95b8175acc2803cd5d77df0c34a3
Nildson <nildsonb15@gmail.com>
2026-06-15 19:14:48 -0300

    "Feat:  Inferface e classe concreta para conversão desserialização do json"

--- src/main/java/com/alura/apifipe/service/ConverteDados.java
+++ src/main/java/com/alura/apifipe/service/ConverteDados.java
@@ -1,11 +1,16 @@
 package com.alura.apifipe.service;
 
 import tools.jackson.core.JacksonException;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 public class ConverteDados implements IConverteDados{
 
     private ObjectMapper mapper = new ObjectMapper();
+
 
     @Override
     public <T> T converteDados(String json, Class<T> classe) {
@@ -15,4 +20,16 @@ public class ConverteDados implements IConverteDados{
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    public <T> List<T> converteLista(String json, Class<T> classe) {
+        CollectionType lista = mapper.getTypeFactory().constructCollectionType(List.class, classe);
+        try {
+            return mapper.readValue(json, lista);
+        } catch (JacksonException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
--- src/main/java/com/alura/apifipe/service/IConverteDados.java
+++ src/main/java/com/alura/apifipe/service/IConverteDados.java
@@ -1,5 +1,9 @@
 package com.alura.apifipe.service;
 
+import java.util.List;
+
 public interface IConverteDados {
     <T> T converteDados(String json, Class<T> classe);
+
+    <T> List<T> converteLista(String json, Class<T> classe);
 }
